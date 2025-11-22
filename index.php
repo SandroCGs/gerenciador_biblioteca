cb7ec934389a0e4bc8948b84753511d8272b8f6d
Sandro <sandrolp41@hotmail.com>
2025-11-22 02:37:36 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+$route = $_GET['route'] ?? 'home';
+
+switch ($route) {
+    case "emprestimo":
+        //! controller chamando emprestimo
+        break;
+
+    case "cadastroItem":
+        //! controller
+        break;
+
+    case "excluiItem":
+
+        break;
+
+    case "cadastroUsuario":
+        break;
+    
+    case "excluiUsuario":
+
+        break;
+
+    case "loginUsuario":
+        
+        break;
+
+    case "loginFuncionario":
+        
+        break;
+    
+    case "devolveItem":
+
+        break;
+
+    default:
+        //home
+        break;
+}
